1eccc841632affe6e1854dd9fe3859348aa1bd81
pandapls <932650719@qq.com>
2024-05-02 17:55:40 +0800

    feat: common_fn 变更

--- packages/common_fn/index.ts
+++ packages/common_fn/index.ts
@@ -3,3 +3,7 @@ import { Direction, playerDirection } from '@lerna_demo_project/common_eum';
 export const getPlayerDirection = (direction: string): Direction | '' => {
   return playerDirection[direction] ? playerDirection[direction] : '';
 };
+
+export const getTest = () => {
+  return 'test';
+};
